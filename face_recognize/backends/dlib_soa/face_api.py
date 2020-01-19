5342f9d8d62ce56c1ac47c2fd939075575ac4dc2
tuyuxiao <754738085@qq.com>
2020-01-19 17:48:08 +0800

    dlib rect order bug

--- face_recognize/backends/dlib_soa/face_api.py
+++ face_recognize/backends/dlib_soa/face_api.py
@@ -5,42 +5,45 @@ from face_recognize.common import FaceInfo
 
 threshold = 0.4
 
+
 def to_rect(face_info):
-    return dlib.rectangle(face_info.top, face_info.right, face_info.bottom, face_info.left)
+    return dlib.rectangle(face_info.left, face_info.top, face_info.right, face_info.bottom)
 
 
 def init_engine(mode='image', num_face=10, mask='all'):
     if mask == 'recognize':
-        #predictor_5_point_model = face_recognition_models.pose_predictor_five_point_model_location()
-        #pose_predictor_5_point = dlib.shape_predictor(predictor_5_point_model)
-        predictor_68_point_model = face_recognition_models.pose_predictor_model_location()
-        pose_predictor_68_point = dlib.shape_predictor(predictor_68_point_model)
+        predictor_5_point_model = face_recognition_models.pose_predictor_five_point_model_location()
+        pose_predictor_5_point = dlib.shape_predictor(predictor_5_point_model)
+        #predictor_68_point_model = face_recognition_models.pose_predictor_model_location()
+        #pose_predictor_68_point = dlib.shape_predictor(predictor_68_point_model)
         face_recognition_model = face_recognition_models.face_recognition_model_location()
         face_encoder = dlib.face_recognition_model_v1(face_recognition_model)
-        return pose_predictor_68_point, face_encoder
+        return pose_predictor_5_point, face_encoder
     else:
-        #face_detector = dlib.get_frontal_face_detector()
-        cnn_face_detection_model = face_recognition_models.cnn_face_detector_model_location()
-        cnn_face_detector = dlib.cnn_face_detection_model_v1(cnn_face_detection_model)
-        return cnn_face_detector
+        face_detector = dlib.get_frontal_face_detector()
+        return face_detector
+        #cnn_face_detection_model = face_recognition_models.cnn_face_detector_model_location()
+        #cnn_face_detector = dlib.cnn_face_detection_model_v1(cnn_face_detection_model)
+        #return cnn_face_detector
 
 
 def detect(engine, img, mode='image',number_of_times_to_upsample=1):
-    img = img[::-1]
-
+    img = img[..., ::-1]
     locations = engine(img,number_of_times_to_upsample)
     face_infos = []
-    for location in locations:
-        face_infos.append(FaceInfo(0, max(location.left(), 0),
-                                   max(location.top(), 0), min(location.right(), img.shape[1]),
-                                   min(location.bottom(), img.shape[0]), 0))
+    for rect in locations:
+        if isinstance(engine, dlib.cnn_face_detection_model_v1):
+            rect = rect.rect
+        face_infos.append(FaceInfo(0, max(rect.left(), 0),
+                                   max(rect.top(), 0), min(rect.right(), img.shape[1]),
+                                   min(rect.bottom(), img.shape[0]), 0))
     return face_infos
 
 
 def extract(engine, img, face_info, *, num_jitters=1):
     pose_predictor, face_encoder = engine
 
-    img = img[::-1]
+    img = img[..., ::-1]
     landmark = pose_predictor(img, to_rect(face_info))
 
     return np.array(face_encoder.compute_face_descriptor(img, landmark, num_jitters))
--- face_recognize/utils.py
+++ face_recognize/utils.py
@@ -1,5 +1,5 @@
 def trim(img):
-    height, width, _ = img.shape
+    height, width = img.shape[0], img.shape[1]
     clip_w = width % 4
     clip_h = height % 2
     if clip_w == 0 and clip_h == 0:
