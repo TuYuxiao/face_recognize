3b783dda69178ff91092d462826ff7d314a12deb
tuyuxiao <754738085@qq.com>
2020-01-29 23:51:39 +0800

    trim image

--- face_recognize/utils.py
+++ face_recognize/utils.py
@@ -22,4 +22,4 @@ def trim(img):
     clip_h = height % 2
     if clip_w == 0 and clip_h == 0:
         return img
-    return img[:width - clip_h, :height - clip_w, :]
+    return img[:height - clip_h, :width - clip_w, :]
