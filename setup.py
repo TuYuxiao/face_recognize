f9ab66ade70ab27c3390890e1e0e430f28490463
tuyuxiao <754738085@qq.com>
2020-01-31 12:45:23 +0800

    version 0.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'face_recognize',
-    version = '0.1.0',
+    version = '0.2.0',
     description = 'face recognize with dlib and arcsoft toolkit',
     license = 'MIT License',
     author = 'Tu Yuxiao',
